6338f4f45ae2ed67c23db8cf75327c7948b4075d
Kyle Chriscoe <kyle717@gmail.com>
2021-01-20 19:47:45 -0500

    working connection to mySQL db

---
+++ server.js
@@ -0,0 +1,18 @@
+// ===DEPENDENCIES===
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+// ===CREATE SQL CONNECTION===
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "V!sionfor20202",
+    database: "hcm_db"
+});
+
+// ===INITIAL CONNECTION TO DB===
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+})
